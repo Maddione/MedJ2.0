64dfa8b90b1d1dffb433f771bdd7a04d53bb1054
TGeorgieva <tgu000000@gmail.com>
2025-06-12 08:12:14 +0300

    - split base.html in base_app and base_public - improved some templates, added basic info - added icons for language change - added profile icon - added a css file for wwebsite colors

--- ocrapi/vision_handler.py
+++ ocrapi/vision_handler.py
@@ -5,10 +5,8 @@ from google.cloud import vision
 from dotenv import load_dotenv
 from google.oauth2 import service_account
 
-# Зареждаме ключа от .env
 load_dotenv()
 
-# credentials path may be undefined in development
 credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 if credentials_path:
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
@@ -18,11 +16,10 @@ else:
 
 
 def extract_text_from_image(image_path):
-    """
-    Извлича текст от изображение чрез Google Cloud Vision API.
-    """
+    """Извлича текст от изображение чрез Google Cloud Vision API."""
     if client is None:
         return ""
+
     with io.open(image_path, 'rb') as image_file:
         content = image_file.read()
 
@@ -48,3 +45,6 @@ def extract_medical_fields_from_text(text):
     results = {}
     for key, pattern in fields.items():
         match = re.search(pattern, text, re.IGNORECASE)
+        if match:
+            results[key] = match.group(1).replace(",", ".")
+    return results
