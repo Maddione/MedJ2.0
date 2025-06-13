372a7aebdc48b1693b652c5d3cce97d7c47e080f
TGeorgieva <tgu000000@gmail.com>
2025-06-13 20:08:26 +0300

    -updated the landing/base_app/upload page - added a warning message to upload/ - added visualization of upload - implimented a two step upload process allowing the user option to edit and approve the text before analizing it -data from upload in now properly stored in the database -switched from bootstrap to tailwind due to conflicts

--- MedJ/urls.py
+++ MedJ/urls.py
@@ -4,18 +4,17 @@ from . import views
 app_name = 'medj'
 
 urlpatterns = [
-    # Публични страници
     path('', views.landing_page, name='landingpage'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.logout_view, name='logout'),
-
-    # Страници след логин
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('upload/', views.upload, name='upload'),
+    path('upload/', views.upload_page, name='upload'),
     path('history/', views.upload_history, name='history'),
     path('casefiles/', views.casefiles, name='casefiles'),
     path('personalcard/', views.personalcard, name='personalcard'),
     path('profile/', views.profile, name='profile'),
     path('doctors/', views.doctors, name='doctors'),
+    path('perform-ocr/', views.perform_ocr, name='perform_ocr'),
+    path('analyze-document/', views.analyze_document, name='analyze_document'),
 ]
