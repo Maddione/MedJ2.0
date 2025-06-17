b6013f5c723afbf48fe56a2f60eaf982c080240e
TGeorgieva <tgu000000@gmail.com>
2025-06-17 12:29:54 +0300

    - settings/ added to github

---
+++ settings/base.py
@@ -0,0 +1,82 @@
+﻿import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+dotenv_path = os.path.join(BASE_DIR, '.env')
+
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path=dotenv_path)
+else:
+    print("Warning: .env file not found.")
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+DEBUG = False
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "MedJ.apps.MedjConfig",
+    "ocrapi",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "MedJ2.urls2"
+WSGI_APPLICATION = "MedJ2.wsgi.application"
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR / 'MedJ' / 'templates' / 'basetemplates',
+            BASE_DIR / 'MedJ' / 'templates',
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
+            ],
+        },
+    },
+]
+
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
+
+LANGUAGE_CODE = "bg"
+LANGUAGES = [('bg', 'Bulgarian'), ('en', 'English')]
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
+TIME_ZONE = "Europe/Sofia"
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_REDIRECT_URL = 'medj:dashboard'
+LOGOUT_REDIRECT_URL = 'medj:landingpage'
+LOGIN_URL = '/login/'
---
+++ settings/prod.py
@@ -0,0 +1,19 @@
+﻿from .base import *
+import os
+
+# --- СИГУРНОСТ ЗА ПРОДУКЦИЯ ---
+DEBUG = False
+
+ALLOWED_HOSTS = ['medj.eu', 'www.medj.eu']
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# --- НАСТРОЙКИ ЗА СИГУРНОСТ ---
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
